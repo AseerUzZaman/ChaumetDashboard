f68138173bb652469b33cb525dd641a414040094
AseerUzZaman <aseer@updot.in>
2021-12-31 18:56:41 +0530

    implement form validation

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,13 @@
+let data = {
+  title: null,
+  firstName: "",
+  lastName: "",
+  province: "",
+  number: "",
+  email: "",
+  date: "",
+};
+
 // Handling the tab switches in home page
 function showSection(evt, tabName) {
   let i, tabContent, tabButton;
@@ -28,7 +38,129 @@ const d = new Date();
 currentDate.innerHTML = d.getDate();
 
 // Selecting calendar date card in overview tab
-function setDate(event) {
+function setDate() {
   const dateText = document.querySelector("#current-date");
   dateText.innerHTML = this.event.target.value.slice(-2);
+}
+
+// Handle title field
+function changeTitle() {
+  if (this.event.target.id === "mr" && this.event.target.checked) {
+    data.title = true;
+    const mrs = document.getElementById("mrs");
+    mrs.checked = false;
+  }
+  if (this.event.target.id === "mrs") {
+    data.title = false;
+    const mr = document.getElementById("mr");
+    mr.checked = false;
+  }
+
+  if (this.event.target.id === "mr" && !this.event.target.checked) {
+    data.title = null;
+    const mr = document.getElementById("mr");
+    mr.checked = false;
+  }
+  if (this.event.target.id === "mrs" && !this.event.target.checked) {
+    data.title = null;
+    const mrs = document.getElementById("mrs");
+    mrs.checked = false;
+  }
+}
+
+// Set first name
+function setFirstName() {
+  data.firstName = this.event.target.value;
+}
+function setLastName() {
+  data.lastName = this.event.target.value;
+}
+function setProvince() {
+  data.province = this.event.target.value;
+}
+function setNumber() {
+  data.number = this.event.target.value;
+}
+function setEmail() {
+  data.email = this.event.target.value;
+}
+function setPickedDate() {
+  data.date = this.event.target.value;
+}
+
+function discard() {
+  const errors = document.querySelectorAll("p.error");
+  errors.forEach((error) => {
+    error.classList.remove("show");
+  });
+
+  data.title = null;
+  data.firstName = "";
+  data.lastName = "";
+  data.province = "";
+  data.number = "";
+  data.email = "";
+  data.date = "";
+}
+
+function validate() {
+  const titleError = document.querySelector("p.error#title");
+  const firstNameError = document.querySelector("p.error#first-name");
+  const lastNameError = document.querySelector("p.error#last-name");
+  const provinceError = document.querySelector("p.error#province");
+  const numberError = document.querySelector("p.error#phone");
+  const emailError = document.querySelector("p.error#email");
+  const dateError = document.querySelector("p.error#date");
+
+  if (data.title === null) {
+    if (!titleError.classList.contains("show"))
+      titleError.classList.add("show");
+  } else {
+    titleError.classList.remove("show");
+  }
+
+  if (data.firstName === "") {
+    if (!firstNameError.classList.contains("show"))
+      firstNameError.classList.add("show");
+  } else {
+    firstNameError.classList.remove("show");
+  }
+
+  if (data.lastName === "") {
+    if (!lastNameError.classList.contains("show"))
+      lastNameError.classList.add("show");
+  } else {
+    lastNameError.classList.remove("show");
+  }
+
+  if (data.province === "") {
+    if (!provinceError.classList.contains("show"))
+      provinceError.classList.add("show");
+  } else {
+    provinceError.classList.remove("show");
+  }
+
+  if (!/^(\+971)?[0-9]{9}$/.test(data.number)) {
+    if (!numberError.classList.contains("show"))
+      numberError.classList.add("show");
+  } else {
+    numberError.classList.remove("show");
+  }
+
+  if (
+    !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      data.email
+    )
+  ) {
+    if (!emailError.classList.contains("show"))
+      emailError.classList.add("show");
+  } else {
+    emailError.classList.remove("show");
+  }
+
+  if (data.date === "") {
+    if (!dateError.classList.contains("show")) dateError.classList.add("show");
+  } else {
+    dateError.classList.remove("show");
+  }
 }
